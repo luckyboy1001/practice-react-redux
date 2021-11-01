5aa40605ec2fe532a2d77946af3a8f52a3677df7
Mr. Programmer lin <m.mohammadi.developer@gmail.com>
2021-11-01 23:28:56 +0330

    until side effects

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,12 +1,23 @@
 import classes from './CartButton.module.css';
+import {useDispatch, useSelector} from "react-redux";
+import {uiActions} from "../../store/ui-slice";
 
 const CartButton = (props) => {
-  return (
-    <button className={classes.button}>
-      <span>My Cart</span>
-      <span className={classes.badge}>1</span>
-    </button>
-  );
+    const dispatch = useDispatch();
+    const cartQuantity = useSelector(state => state.cart.totalQuantity);
+
+
+
+    const toggleCardHandler = () => {
+        dispatch(uiActions.toggle());
+    }
+
+    return (
+        <button className={classes.button} onClick={toggleCardHandler}>
+            <span>My Cart</span>
+            <span className={classes.badge}>{cartQuantity}</span>
+        </button>
+    );
 };
 
 export default CartButton;
--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,35 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const PRODUCTS = [
+    {
+        id: 1,
+        price: 8,
+        title: 'First Product',
+        description: 'The First Book Description'
+    },
+    {
+        id: 2,
+        price: 10,
+        title: 'Second Product',
+        description: 'The Second Book Description'
+    }
+];
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+          {PRODUCTS.map(item =>
+              <ProductItem
+                  key={item.id}
+                  id={item.id}
+                  title={item.title}
+                  price={item.price}
+                  description={item.description}
+              />
+          )}
       </ul>
     </section>
   );
